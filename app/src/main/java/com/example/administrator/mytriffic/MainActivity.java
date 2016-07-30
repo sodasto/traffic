78a61c82ec2474bffe7de6652c0e2f7f14f24de6
sodasto <sodasto.kino@gmail.com>
2016-07-30 15:02:52 +0700

    finish 30 july

--- app/src/main/java/com/example/administrator/mytriffic/MainActivity.java
+++ app/src/main/java/com/example/administrator/mytriffic/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.mytriffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }//Main Method
+
+    public void clickAboutMe(View view) {
+
     }
-}
+
+
+
+} // Main Class
