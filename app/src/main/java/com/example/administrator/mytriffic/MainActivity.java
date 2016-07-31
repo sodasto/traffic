d67f852c60d354dc7867bc7e5789689d3eee6bed
sodasto <sodasto.kino@gmail.com>
2016-07-31 14:25:15 +0700

    List view OK

--- app/src/main/java/com/example/administrator/mytriffic/MainActivity.java
+++ app/src/main/java/com/example/administrator/mytriffic/MainActivity.java
@@ -8,6 +8,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
 import java.security.PrivateKey;
 
@@ -16,12 +17,55 @@ public class MainActivity extends AppCompatActivity {
     //Explicit การประกาศตัวแปล มี แบบ สาธารณะ กับ ส่วนตัว
 
     private String urlYoutubeString;
+    private ListView listView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        //Setup array for integer
+        int[] iconInts = new int[20];
+        iconInts[0] = R.drawable.traffic_01;
+        iconInts[1] = R.drawable.traffic_02;
+        iconInts[2] = R.drawable.traffic_03;
+        iconInts[3] = R.drawable.traffic_04;
+        iconInts[4] = R.drawable.traffic_05;
+        iconInts[5] = R.drawable.traffic_06;
+        iconInts[6] = R.drawable.traffic_07;
+        iconInts[7] = R.drawable.traffic_08;
+        iconInts[8] = R.drawable.traffic_09;
+        iconInts[9] = R.drawable.traffic_10;
+        iconInts[10] = R.drawable.traffic_11;
+        iconInts[11] = R.drawable.traffic_12;
+        iconInts[12] = R.drawable.traffic_13;
+        iconInts[13] = R.drawable.traffic_14;
+        iconInts[14] = R.drawable.traffic_15;
+        iconInts[15] = R.drawable.traffic_16;
+        iconInts[16] = R.drawable.traffic_17;
+        iconInts[17] = R.drawable.traffic_18;
+        iconInts[18] = R.drawable.traffic_19;
+        iconInts[19] = R.drawable.traffic_20;
+
+        //For Setup Title
+        String[] titleStrings = getResources().getStringArray(R.array.my_title);
+
+        String[] detailLongStrings = getResources().getStringArray(R.array.my_datail);
+
+        String[] detailShot = new String[detailLongStrings.length];
+        for (int i=0;i<detailLongStrings.length;i+=1) {
+            detailShot[i]= detailLongStrings[i].substring(0, 20) + "..."; // "..." ให้ตัดคำที่20 ตัวอักษรหลัง 20 ให้ .. ต่อไป
+
+        }
+
+        //Create ListView
+        MyAdapter myAdapter = new MyAdapter(this, iconInts, titleStrings, detailShot);
+        listView.setAdapter(myAdapter);
+
+
     }//Main Method
 
 
@@ -40,15 +84,7 @@ public class MainActivity extends AppCompatActivity {
         // Shift command enter สำหรับเติมเครื่องหมายปิดคำสั่งเอง แทน : หรือ ;
 
 
-
-
-
-
-
-
-
     }
-
 
 
 } // Main Class
