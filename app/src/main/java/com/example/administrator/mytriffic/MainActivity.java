6df98c191f150b25ed4d0197545344bec74f5476
sodasto <sodasto.kino@gmail.com>
2016-07-31 10:59:05 +0700

    intent to Webview

--- app/src/main/java/com/example/administrator/mytriffic/MainActivity.java
+++ app/src/main/java/com/example/administrator/mytriffic/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.administrator.mytriffic;
 
+import android.content.Intent;
+import android.hardware.camera2.params.RggbChannelVector;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import java.security.PrivateKey;
+
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit การประกาศตัวแปล มี แบบ สาธารณะ กับ ส่วนตัว
+
+    private String urlYoutubeString;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +26,26 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void clickAboutMe(View view) {
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.effect_btn_long);
+        mediaPlayer.start();
+
+        // intent to webview
+
+        urlYoutubeString = "https://youtu.be/AWMqoSJfJMc";
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(urlYoutubeString));
+        startActivity(intent);
+
+        // Shift command enter สำหรับเติมเครื่องหมายปิดคำสั่งเอง แทน : หรือ ;
+
+
+
+
+
+
+
+
 
     }
 
